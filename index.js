11ff301d638a45d26d97488b017a61bbd11fa08d
dangquangvu <dangquangvu999@gmail.com>
2019-09-14 18:12:36 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+var Validator = require('jsonschema').Validator;
+var v = new Validator();
+
+var UserSchema = {
+    "id": "/SimpleAddress",
+    "title": "User",
+    "description": "User profile",
+    "type": "object",
+    "properties": {
+        "name": {
+            "type": "string",
+            "enum": ["Tran Van A", "Tran Van B"],
+        },
+        "date": {
+            "type": "string",
+            "format": "date"
+        },
+        "email": {
+            "type": "string",
+            "format": "email",
+            "minLength": 0,
+            "maxLength": 180,
+        },
+        "telephoneNumber": {
+            "type": "number",
+            "pattern": "(09|01[2|6|8|9])+([0-9]{8})",
+            "minimum": 9,
+            "maximum": 11,
+        },
+        "GPA": {
+            "type": "number",
+            "minimum": 0,
+            "maximum": 4
+        }
+    },
+    "required": ["name", "date", "email", "telephoneNumber", "GPA"]
+}
+
+
+/**
+ * name # enum
+date  
+email
+sdt
+thang điểm # hs:4
+{
+    name :
+    date:
+    email:
+    sdt:
+    gpa:
+}
+ */
